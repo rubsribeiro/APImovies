b66aa282354043a7ffe1a7582c95e7f9e0104133
Rubia <rubia.n.ribeiro@gmail.com>
2023-09-22 22:43:31 -0300

    aplicando estilo e funcionalidade do botão Go Back

--- src/pages/Home/Details/index.js
+++ src/pages/Home/Details/index.js
@@ -1,16 +1,51 @@
-import { useParams } from "react-router-dom";
-
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import { Container } from "../Details/style";
 
 function Details() {
 
+    const { id }= useParams()
+    
+    const [movie, setMovie] = useState({})
+    const image_path = 'https://image.tmdb.org/t/p/w500';
 
-    const params = useParams()
-    console.log(params.id)
+        useEffect(() => {
+
+            fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=6fbc07760a3d3f9d3a49b81884939815&language=en-US&page=1`)
+            
+            .then(response => response.json())
+            .then(data => {
+
+                const {title, poster_path, overview, release_date } = data
+                const movie = {
+                    id,
+                    title: title,
+                    sinopse: overview,
+                    image: `${image_path}${poster_path}`,
+                    releaseDate: release_date
+                
+    }
+
+        setMovie(movie)
+    })
+
+}, [id])
 
     return (
-        <h1>Details</h1>
+        <Container>
+            <div className="movie">
+                <img src={movie.image} alt={movie.sinopse}/>
+                <div className="details">
+                    <h1>{movie.title}</h1>
+                    <span>Sinopse: {movie.sinopse}</span>
+                    <span className="release-date">Release date: {movie.releaseDate}</span>
+                    <Link to="/"><button>Go Back</button></Link>
+                    
+                </div>
+            </div>
+        </Container>
 
     )
 }
 
-export default Details;
+export default Details
